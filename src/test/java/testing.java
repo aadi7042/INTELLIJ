d1687a2795d38ca9aa21875c338aafde3ea6da3c
Aditya <shivambabu5@gmail.com>
2023-12-15 23:02:56 +0530

    TestNg integrated

--- src/test/java/testing.java
+++ src/test/java/testing.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.support.ui.Select;
@@ -13,38 +14,46 @@ import java.lang.*;
 
 import org.testng.Assert;
 import org.testng.annotations.*;
-
 public class testing {
     WebDriver driver;
     @BeforeSuite
     public void setup() {
+        //ChromeOptions options = new ChromeOptions();
+        //options.addArguments("--headless=new");
+        ////USE THE ABOVE TWO LINES FOR EXECUTION OF CASES WITHOUT GUI
          driver = new ChromeDriver();
         System.out.println("This is before suite : SETTING UP WEBDRIVER");
     }
+    @BeforeTest
+     public Void beforetest(){
+        System.out.println("this is before test");
+        return null;
+    }
     @BeforeClass
     public void beforeclass(){
-        driver = new ChromeDriver();
+        System.out.println("this is before class");
 
     }
     @BeforeMethod
     public void beforemethod(){
         System.out.println("This is before method");
     }
-    @Test
-    public void test1(){
+    @Test()
+    public void test1() throws InterruptedException{
         System.out.println("this is test case1");
-        driver.get("https://www.amazon.com");
+        driver.get("https://www.Google.com");
+        Thread.sleep(2000);
         String title=driver.getTitle();
         System.out.println(title);
-        Assert.assertEquals(title,"Amazon.com. Spend less. Smile more.");
+        Assert.assertEquals(title,"Google");
     }
-    @Test()
+    @Test(dependsOnMethods = "test1")
     public void test2(){
         System.out.println("this is test case 2");
-        driver.get("https://www.amazon.com");
+        driver.get("https://www.Youtube.com");
         String title=driver.getTitle();
         System.out.println(title);
-        Assert.assertEquals(title,"Amazon.com. Spend less. Smile more.");
+        Assert.assertEquals(title,"YouTube");
     }
     @AfterMethod
     public void aftermethod(){
@@ -54,6 +63,12 @@ public class testing {
     public void Afterclass(){
         System.out.println("this is After class");
 
+
+    }
+    @AfterTest
+     public Void Aftertest(){
+        System.out.println("this is after test");
+        return null;
     }
     @AfterSuite
     public void close(){
